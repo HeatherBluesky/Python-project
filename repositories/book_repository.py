fac7a42b1812e3e38827d27af8ad971c6455ee36
Heather <heather@Kierans-MacBook.local>
2023-04-15 12:37:12 +0100

    book repository update

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -18,3 +18,30 @@ def select_all():
         book = Book (row['title'], author, row['genre'], row['quantity'], row ['buying_price'], row['selling_price'], row['language'])
         books.append(book)
     return books
+
+def update(book):
+    sql = "UPDATE books SET ( title, author_id, genre, quanity, buying_price, selling_price, language) = (%s, %s, %s, %s, %s, %s, %s) WHERE id = %s"
+    values = [ book.title, book.author,id, book.genre, book.quanity, book.buying_price, book.selling_price, book.language, book.id]
+    run_sql(sql, values)
+
+def get_langauges(books):
+    books = []
+    book_language = book_language
+    sql = "SELECT * FROM  books WHERE language = %s"
+    values = [book_language]
+    results = run_sql(sql, values)
+    for row in results:
+        book = Book(row['title'], row['author'], row['genre'], row['quanity'], row['buying_price'], row['selling_price'], row['language'], row['id'])
+        books.append(book)
+    return books
+
+
+def delete(id):
+    sql = "DELETE FROM books where id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def delete_all():
+    sql = "DELETE * from books"
+    run_sql(sql)
